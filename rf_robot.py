5a0dd2ffa5fd613973fc83d13b33dd1b914a8e04
Souvik Datta <souvikdatta123@gmail.com>
2022-10-16 22:52:13 +0530

    initial commit

---
+++ rf_robot.py
@@ -0,0 +1,35 @@
+# !pip install pybullet
+
+# !pip install gym
+
+# !pip install gym[box2d]
+
+# !pip install stable-baselines3
+
+import gym
+import pybullet
+import pybullet_envs
+import torch as th
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+
+env = gym.make("AntBulletEnv-v0")
+
+env.render(mode='human')
+
+MAX_AVG_SCORE = 2000
+
+policy_kwargs = dict(activation_fn=th.nn.LeakyReLU,net_arch=[512,512])
+
+model = PPO('MlpPolicy', env, learning_rate=0.003, policy_kwargs=policy_kwargs, verbose=1)
+
+for i in range(8000):
+  print("Traning iteration ", i)
+  model.learn(total_timesteps=10000)
+  model.save("ppo_Ant_saved_model_4")
+  mean_reward, std_reward = evaluate_policy(model,model.get_env(),n_eval_episodes=15)
+  print("mean reward ", mean_reward)
+  if mean_reward >= MAX_AVG_SCORE:
+    break
+
+# def modelf
